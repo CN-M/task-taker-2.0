443d46ec3722e9b561d946c4b0829d4abe3b703b
C.N. <cn.mbhalati@gmail.com>
2024-06-26 13:59:40 +0200

    remove dist directory

--- dist/client/src/pages/Login.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Login = void 0;
-const loginForm_1 = require("../components/loginForm");
-const Login = () => {
-    return <loginForm_1.LoginForm />;
-};
-exports.Login = Login;
--- dist/client/src/pages/Register.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Register = void 0;
-const signUpForm_1 = require("../components/signUpForm");
-const Register = () => {
-    return <signUpForm_1.SignUpForm />;
-};
-exports.Register = Register;
